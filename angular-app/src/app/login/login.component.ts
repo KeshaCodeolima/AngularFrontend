6f1b0fdabed96d5259b6e8028199fb8102017869
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-01 11:43:03 +0530

    set the connection with backend and give the from submit passind values

--- angular-app/src/app/login/login.component.ts
+++ angular-app/src/app/login/login.component.ts
@@ -1,12 +1,29 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  email: String ='';
+  password: String='';
+
+  constructor(private http:HttpClient){}
+
+  onLogin(){
+    const LoginData ={
+      email:this.email,
+      password:this.password,
+    };
+    this.http.post('http://127.0.0.1:5000/login',LoginData).subscribe(res =>{
+      console.log("Login Successful",res);
+    },error =>{console.log("Login Unsuccessful",error);
+    });
+  }
 }
