93e5fcc3887285d7cc8397ef934ce939b78011fa
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-01 12:22:54 +0530

    change the opening page to home page

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'register', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component:HomeComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent }
