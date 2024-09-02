0029510a559bab52733dea70ac8616dce59a3850
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-02 09:30:35 +0530

    Add the routers.ts file to forget page router

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ForgetComponent } from './forget/forget.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component:HomeComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    {path:'froget', component:ForgetComponent}
 ];
