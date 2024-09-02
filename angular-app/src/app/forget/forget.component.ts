3c9188afa4341a96e5566f4f602ecd462f8b8e1d
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-02 12:23:08 +0530

    After change the password Add router to go to the login page

--- angular-app/src/app/forget/forget.component.ts
+++ angular-app/src/app/forget/forget.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -14,7 +15,7 @@ export class ForgetComponent {
   email:String='';
   new_password:String='';
 
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient, private router:Router){}
 
   onforget(){
     const forgetData ={
@@ -24,6 +25,7 @@ export class ForgetComponent {
     this.http.post('http://127.0.0.1:5000/update',forgetData).subscribe(res=>{
       console.log("Successfully Change the password",res);
       alert("Successfully Change the password");
+      this.router.navigate(['/login'])
     },error=>{console.log("password is not change ",error);
     });
   }
