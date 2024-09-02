b2a510f37cf8eeac3ff79aed34afdd9e68efda95
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-02 11:24:33 +0530

    add the backend part to forget page

--- angular-app/src/app/forget/forget.component.ts
+++ angular-app/src/app/forget/forget.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './forget.component.css'
 })
 export class ForgetComponent {
+  email:String='';
+  password:String='';
 
+  constructor(private http:HttpClient){}
+
+  onforget(){
+    const forgetData ={
+      email:this.email,
+      password:this.password
+    };
+    this.http.post('http://127.0.0.1:5000/update',forgetData).subscribe(res=>{
+      console.log("Successfully Change the password",res);
+      alert("Successfully Change the password");},
+      error=>{console.log("password is not change ",error);
+      })
+  }
 }
