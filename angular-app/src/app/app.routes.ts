433e9c54b48f067a69d4986b44932b46cdab3287
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-04 09:06:27 +0530

    Add the router link to contact page

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ForgetComponent } from './forget/forget.component';
 import { AdminComponent } from './admin/admin.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -11,5 +12,6 @@ export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     {path:'froget', component:ForgetComponent},
-    {path:'admin', component:AdminComponent}
+    {path:'admin', component:AdminComponent},
+    {path:'contact', component:ContactComponent}
 ];
