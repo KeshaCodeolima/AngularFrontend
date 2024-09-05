6aa5e59eee143a52bb6f3123c73b0918ca635aa9
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-05 12:12:31 +0530

    change the forget email page link logic

--- angular-app/src/app/forgetemail/forgetemail.component.ts
+++ angular-app/src/app/forgetemail/forgetemail.component.ts
@@ -19,11 +19,20 @@ export class ForgetemailComponent {
     const senddata = {
       email:this.email
     };
-    this.http.post('http://127.0.0.1:5000/forgetemail',senddata).subscribe(res=>{
-      console.log("Email Send Successful",res);
-      alert("Forget password page link send to Your Email.");
-    },error=>{console.log("Your Email is not send",error);
-      alert("Your Email is not Send Check Your Email")
-    })
+    this.http.post<any>('http://127.0.0.1:5000/forgetemail',senddata).subscribe(
+      res=>{
+        if(res.message === "Email Send Successful"){
+          console.log("Email Send Successful",res);
+          alert("Forget password page link sent to your email.");
+        } else if (res.message === 'Email not found.') {
+          console.log('Email not found', res);
+          alert('The email address is not registered.');
+        }
+      },
+      error => {
+        console.log('Error occurred:', error);
+        alert('An error occurred. Please try again later.');
+      }
+    );
   }
 }
