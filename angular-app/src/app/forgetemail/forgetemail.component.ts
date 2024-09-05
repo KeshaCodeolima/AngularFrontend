e8f1227fb36041f4e0a90c746a8732170257d083
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-05 11:36:56 +0530

    create the forget email page to send the link of the forget password page

---
+++ angular-app/src/app/forgetemail/forgetemail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgetemail',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgetemail.component.html',
+  styleUrl: './forgetemail.component.css'
+})
+export class ForgetemailComponent {
+
+}
