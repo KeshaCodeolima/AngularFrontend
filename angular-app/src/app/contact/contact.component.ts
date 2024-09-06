4f9502cc1e7a49b10d3b1c7e79b145432a2b398a
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-06 10:14:42 +0530

    connect with the emailJs

--- angular-app/src/app/contact/contact.component.ts
+++ angular-app/src/app/contact/contact.component.ts
@@ -9,4 +9,19 @@ import emailjs, {EmailJSResponseStatus} from 'emailjs-com';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_oy4at6h', 'template_xw6se89', e.target as HTMLFormElement,'6B8ivzkQOW-eECsqa')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 }
