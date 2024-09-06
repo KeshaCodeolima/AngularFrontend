f543b1046e7a2dbb2abe20db3b21b083244956f1
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-09-06 11:29:31 +0530

    Add the link to send the mail using the python backend

--- angular-app/src/app/contact/contact.component.ts
+++ angular-app/src/app/contact/contact.component.ts
@@ -1,27 +1,33 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import emailjs, {EmailJSResponseStatus} from 'emailjs-com';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  
+  name:String='';
+  email:String='';
+  message:String='';
 
-  public sendEmail(e: Event) {
-    e.preventDefault();
+  constructor(private http:HttpClient){}
 
-    emailjs
-      .sendForm('service_oy4at6h', 'template_xw6se89', e.target as HTMLFormElement,'6B8ivzkQOW-eECsqa')
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', (error as EmailJSResponseStatus).text);
-        },
-      );
+  onsubmit(){
+    const maildata= {
+      name:this.name,
+      email:this.email,
+      message:this.message,
+    };
+    this.http.post("http://127.0.0.1:5000/usercomment",maildata).subscribe(res=>{
+      console.log("Mail Send Succeful",res);
+      alert("Your Email Send Successful");
+    },error=>{console.log("Mail not send Successful",error);
+    });
   }
 }
